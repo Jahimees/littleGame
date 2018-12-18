d673ddf00ad27098523dff89cefc3585dcb42400
TheFearka <JerryMous1901@gmail.com>
2018-12-18 17:10:24 +0300

    added subclasses to class equipment (boots, breastplate, helmet, leggins, shield, weapon)

--- src/Equipment.java
+++ src/Equipment.java
@@ -1,4 +1,36 @@
 
 public class Equipment {
-
+	
+	
+	
+	
+	class Breastplate {
+		String name;
+		int armor;		
+	}
+	
+	class Helmet {
+		String name;
+		int armor;
+	}
+	
+	class Leggins {
+		String name;
+		int armor;
+	}
+	
+	class Boots {
+		String name;
+		int armor;
+	}
+	
+	class Shield {
+		String name;
+		int armor;
+	}
+	
+	class Weapon {
+		String name;
+		int damage;
+	}
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Player {
+	ArrayList inventory = new ArrayList();
+	private int health;
+	
+	
+	////////////////////////GETTERS AND SETTERS////////////////////
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	
 
 }
